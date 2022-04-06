4d844082e0e87083a2966ee3ab371aad9a7cc2d7
edavis56 <92826817+edavis56@users.noreply.github.com>
2022-04-05 20:23:44 -0400

    Created and setup initial services model

---
+++ server/models/services.js
@@ -0,0 +1,20 @@
+const { Schema } = require('mongoose');
+
+const servicesSchema = new Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = servicesSchema;
