18450fea23907ff9f3f45db7ed77088cfe37ffdc
Jude Collins <jude.collins13@yahoo.com>
2022-04-14 20:59:43 -0400

    debugging

--- client/src/screens/productDescScreen.js
+++ client/src/screens/productDescScreen.js
@@ -18,7 +18,7 @@ console.log(match)
             <p>{product.description}</p>
           </div>
         </div>
-
+giut pu
         <div className="col-md-6 text-left">
           <div className="m-2">
             <h1>Price: {product.price}</h1>
